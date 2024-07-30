57fc268d2b7bc41782476365207a9735bcc73cdc
mohammadA19 <48352583+mohammadA19@users.noreply.github.com>
2024-07-30 19:53:27 +0300

    doc comments

--- stb_rect_pack.h
+++ stb_rect_pack.h
@@ -71,31 +71,6 @@ def Coord = int;
 #define STBRP__MAXVAL  0x7fffffff
 // Mostly for internal use, but this is the maximum supported coordinate value.
 
-int pack_rects (Context* context, Rect *rects, int num_rects);
-// Assign packed locations to rectangles. The rectangles are of type
-// 'rect' defined below, stored in the array 'rects', and there
-// are 'num_rects' many of them.
-//
-// Rectangles which are successfully packed have the 'was_packed' flag
-// set to a non-zero value and 'x' and 'y' store the minimum location
-// on each axis (i.e. bottom-left in cartesian coordinates, top-left
-// if you imagine y increasing downwards). Rectangles which do not fit
-// have the 'was_packed' flag set to 0.
-//
-// You should not try to access the 'rects' array from another thread
-// while this function is running, as the function temporarily reorders
-// the array while it executes.
-//
-// To pack into another rectangle, you need to call init_target
-// again. To continue packing into the same rectangle, you can call
-// this function again. Calling this multiple times with multiple rect
-// arrays will probably produce worse packing results than calling it
-// a single time with the full rectangle array, but the option is
-// available.
-//
-// The function returns 1 if all of the rectangles were successfully
-// packed and 0 otherwise.
-
 struct Rect
 {
    // reserved for your use:
@@ -109,39 +84,6 @@ struct Rect
    int      was_packed;  // non-zero if valid packing
 
 } // 16 bytes, nominally
-
-
-void init_target (Context* context, int width, int height, Node* nodes, int num_nodes);
-// Initialize a rectangle packer to:
-//    pack a rectangle that is 'width' by 'height' in dimensions
-//    using temporary storage provided by the array 'nodes', which is 'num_nodes' long
-//
-// You must call this function every time you start packing into a new target.
-//
-// There is no "shutdown" function. The 'nodes' memory must stay valid for
-// the following pack_rects() call (or calls), but can be freed after
-// the call (or calls) finish.
-//
-// Note: to guarantee best results, either:
-//       1. make sure 'num_nodes' >= 'width'
-//   or  2. call allow_out_of_mem() defined below with 'allow_out_of_mem = true'
-//
-// If you don't do either of the above things, widths will be quantized to multiples
-// of small integers to guarantee the algorithm doesn't run out of temporary storage.
-//
-// If you do #2, then the non-quantized algorithm will be used, but the algorithm
-// may run out of temporary storage and be unable to pack some rectangles.
-
-void setup_allow_out_of_mem (Context* context, bool allow_out_of_mem);
-// Optionally call this function after init but before doing any packing to
-// change the handling of the out-of-temp-memory scenario, described above.
-// If you call init again, this will be reset to the default (false).
-
-
-void setup_heuristic (Context* context, int heuristic);
-// Optionally select which packing heuristic the library should use. Different
-// heuristics will produce better/worse results for different data sets.
-// If you call init again, this will be reset to the default.
 
 enum
 {
@@ -201,6 +143,11 @@ enum
    STBRP__INIT_skyline = 1
 };
 
+/**
+ * Optionally select which packing heuristic the library should use. Different
+ * heuristics will produce better/worse results for different data sets.
+ * If you call init again, this will be reset to the default.
+ */
 void setup_heuristic(Context* context, int heuristic)
 {
    switch (context.init_mode) {
@@ -212,6 +159,11 @@ void setup_heuristic(Context* context, int heuristic)
    }
 }
 
+/**
+ * Optionally call this function after init but before doing any packing to
+ * change the handling of the out-of-temp-memory scenario, described above.
+ * If you call init again, this will be reset to the default (false).
+ */
 void setup_allow_out_of_mem(Context* context, bool allow_out_of_mem)
 {
    if (allow_out_of_mem)
@@ -232,6 +184,27 @@ void setup_allow_out_of_mem(Context* context, bool allow_out_of_mem)
    }
 }
 
+/**
+ * Initialize a rectangle packer to:
+ *    pack a rectangle that is 'width' by 'height' in dimensions
+ *    using temporary storage provided by the array 'nodes', which is 'num_nodes' long
+ *
+ * You must call this function every time you start packing into a new target.
+ *
+ * There is no "shutdown" function. The 'nodes' memory must stay valid for
+ * the following pack_rects() call (or calls), but can be freed after
+ * the call (or calls) finish.
+ *
+ * Note: to guarantee best results, either:
+ *       1. make sure 'num_nodes' >= 'width'
+ *   or  2. call allow_out_of_mem() defined below with 'allow_out_of_mem = true'
+ *
+ * If you don't do either of the above things, widths will be quantized to multiples
+ * of small integers to guarantee the algorithm doesn't run out of temporary storage.
+ *
+ * If you do #2, then the non-quantized algorithm will be used, but the algorithm
+ * may run out of temporary storage and be unable to pack some rectangles.
+ */
 void init_target(Context* context, int width, int height, Node* nodes, int num_nodes)
 {
    int i;
@@ -512,6 +485,31 @@ static int rect_original_order(const void* a, const void* b)
    return (p.was_packed < q.was_packed) ? -1 : (p.was_packed > q.was_packed);
 }
 
+/**
+ * Assign packed locations to rectangles. The rectangles are of type
+ * 'rect' defined below, stored in the array 'rects', and there
+ * are 'num_rects' many of them.
+ *
+ * Rectangles which are successfully packed have the 'was_packed' flag
+ * set to a non-zero value and 'x' and 'y' store the minimum location
+ * on each axis (i.e. bottom-left in cartesian coordinates, top-left
+ * if you imagine y increasing downwards). Rectangles which do not fit
+ * have the 'was_packed' flag set to 0.
+ *
+ * You should not try to access the 'rects' array from another thread
+ * while this function is running, as the function temporarily reorders
+ * the array while it executes.
+ *
+ * To pack into another rectangle, you need to call init_target
+ * again. To continue packing into the same rectangle, you can call
+ * this function again. Calling this multiple times with multiple rect
+ * arrays will probably produce worse packing results than calling it
+ * a single time with the full rectangle array, but the option is
+ * available.
+ *
+ * The function returns 1 if all of the rectangles were successfully
+ * packed and 0 otherwise.
+ */
 int pack_rects(Context* context, Rect* rects, int num_rects)
 {
    int i, all_rects_packed = 1;
